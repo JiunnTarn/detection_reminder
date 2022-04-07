08cc0cb0b45b57ea5bb5e0abfd5cc4fe6ddb60d9
JiunnTarn <aencil@yeah.net>
2022-04-07 16:36:15 +0800

    加入ActionCard

--- src/main/java/com/jiunntarn/detection_reminder/service/ReminderService.java
+++ src/main/java/com/jiunntarn/detection_reminder/service/ReminderService.java
@@ -1,5 +1,6 @@
 package com.jiunntarn.detection_reminder.service;
 
+import com.dingtalk.api.response.OapiRobotSendResponse;
 import com.jiunntarn.detection_reminder.Dao.DAO;
 import com.jiunntarn.detection_reminder.setting.Settings;
 import com.jiunntarn.detection_reminder.setting.Time;
@@ -10,10 +11,7 @@ import com.dingtalk.api.DingTalkClient;
 import com.dingtalk.api.request.OapiRobotSendRequest;
 import com.jiunntarn.detection_reminder.util.TimeUtil;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author JiunnTarn
@@ -22,54 +20,59 @@ public class ReminderService {
 
     public static String reminder() throws Exception {
         TimeUtil.printTime();
-        System.out.println("\tRan reminder");
+        System.out.println("\tRun reminder");
 
         DAO dao = new DAO();
 
         Settings settings = new Settings(getTime());
         String who = settings.getWho() == Who.ZJJ ? "朱嘉俊" : "徐宇翔";
-        String textContent = "";
+        String textContent;
         DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?access_token=99d3f226f6770bc23f77ac9794ffed569ae4886192865bf5def4a2e5c5de58e2" + SignUtil.sign());
         OapiRobotSendRequest request = new OapiRobotSendRequest();
 
         List<String> atList = dao.getAtList(settings);
-        if(!atList.isEmpty()) {
-            request.setMsgtype("markdown");
-            OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
+
+        if (!atList.isEmpty()) {
             if (getTime() == Time.MORNING) {
-                markdown.setTitle("今天这些同学需要做核酸");
+                request.setMsgtype("actionCard");
+                OapiRobotSendRequest.Actioncard actioncard = new OapiRobotSendRequest.Actioncard();
+                actioncard.setTitle("今天这些同学需要做核酸");
                 textContent = "### 今天这些同学需要做核酸\n" + "### 确认好了跟" + who + "说下[天使]\n\n" + "> " + dao.getAtString(settings) + "\n";
+                actioncard.setBtnOrientation("0");
+                OapiRobotSendRequest.Btns btns = new OapiRobotSendRequest.Btns();
+                btns.setTitle("(test)我确认了");
+                btns.setActionURL("http://www.baidu.com/");
+                actioncard.setText(textContent);
+                actioncard.setBtns(Arrays.asList(btns));
+                request.setActionCard(actioncard);
             } else if (getTime() == Time.AFTERNOON) {
+                request.setMsgtype("markdown");
+                OapiRobotSendRequest.Markdown markdown = new OapiRobotSendRequest.Markdown();
                 markdown.setTitle("明天这些同学需要做核酸");
                 textContent = "### 明天这些同学需要做核酸\n\n" + "> " + dao.getAtString(settings) + "\n";
+                markdown.setText(textContent);
+                request.setMarkdown(markdown);
             }
-            markdown.setText(textContent);
-            request.setMarkdown(markdown);
 
             OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
             at.setAtMobiles(atList);
             request.setAt(at);
 
-            System.out.println();
-            System.out.print("at: ");
-            System.out.println(request.getAt());
-
+            OapiRobotSendResponse response = client.execute(request);
+            if (response.isSuccess()) {
+                System.out.println("Done");
+            } else {
+                System.out.println("Error");
+            }
             return request.getAt();
-
-//        OapiRobotSendResponse response = client.execute(request);
-//        if (response.isSuccess()) {
-//            System.out.println("Done");
-//        } else {
-//            System.out.println("Error");
-//        }
         } else {
             return "Already Done.";
         }
     }
 
     public static Time getTime() {
-        Calendar cal=Calendar.getInstance();
-        int h=cal.get(Calendar.HOUR_OF_DAY);
+        Calendar cal = Calendar.getInstance();
+        int h = cal.get(Calendar.HOUR_OF_DAY);
         if (h < 17) {
             return Time.MORNING;
         } else if (h >= 17) {
