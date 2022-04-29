7df830abf958c62ffe88125c4c83780c861a38c9
JiunnTarn <aencil@yeah.net>
2022-04-29 10:12:36 +0800

    修改html。更改提醒时间。

--- src/main/java/com/jiunntarn/detection_reminder/controller/SubmitController.java
+++ src/main/java/com/jiunntarn/detection_reminder/controller/SubmitController.java
@@ -5,6 +5,7 @@ import com.jiunntarn.detection_reminder.service.ExcludeService;
 import com.jiunntarn.detection_reminder.service.SubmitService;
 import com.jiunntarn.detection_reminder.util.TimeUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ import java.io.IOException;
  * @author JiunnTarn
  */
 @Controller
-@RequestMapping(value = "/submit",method = RequestMethod.GET)
+@RequestMapping(value = "/submit", method = RequestMethod.GET)
 public class SubmitController {
 
     @GetMapping("/")
@@ -21,22 +22,37 @@ public class SubmitController {
         return "index";
     }
 
-    @ResponseBody
     @GetMapping("/index.html")
-    public String submit(@RequestParam("name") String name, @RequestParam("u") String u, @RequestParam("p") String p) throws IOException {
+    public String submit(Model model, @RequestParam("name") String name, @RequestParam("u") String u, @RequestParam("p") String p) throws IOException {
         TimeUtil.printTime();
         System.out.println("\t" + name + " submit.");
 
-        String message = "";
+        String status, message = "";
 
-        message = SubmitService.submitService(name,u,p);
+        message = SubmitService.submitService(name, u, p);
+        model.addAttribute("msg", message);
         int code = ExcludeService.excludeByName(name);
         switch (code) {
-            case 0 -> message += "<br>" + name + " 提交成功。";
-            case 1 -> message += "<br>" +"找不到 " + name + "。";
-            case 2 -> message += "<br>" + name + " 今天已经提交过了。";
-            default -> message += "<br>" + "error";
+            case 0: {
+                status = name + " 提交成功。";
+                model.addAttribute("status", status);
+                return "done";
+            }
+            case 1: {
+                status = "找不到 " + name + "。";
+                model.addAttribute("status", status);
+                return "retry";
+            }
+            case 2: {
+                status = name + " 今天已经提交过了。";
+                model.addAttribute("status", status);
+                return "retry";
+            }
+            default: {
+                status = "未知错误";
+                model.addAttribute("status", status);
+                return "retry";
+            }
         }
-        return message;
     }
 }
